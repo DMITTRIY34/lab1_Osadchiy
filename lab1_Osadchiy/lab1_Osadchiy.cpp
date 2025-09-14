ca4450e96966eccdc319770d5917ed43bc44cb6a
Dmitriy <109093389+DMITTRIY34@users.noreply.github.com>
2025-09-14 20:25:31 +0300

    Меню и валидация ввода чисел
    Доделал меню
    Реализовал функции ввода целочисленных и вещественных чисел

--- lab1_Osadchiy/lab1_Osadchiy.cpp
+++ lab1_Osadchiy/lab1_Osadchiy.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <limits>
 
 
 using namespace std;
@@ -6,24 +7,69 @@ using namespace std;
 
 struct Pipe
 {
-    string name;
-    float length;
-    int diameter;
-    bool inRepair;
+    string name; // название
+    float length; // длина
+    int diameter; // диаметр
+    bool inRepair; // в ремонте
 };
 
 struct Ks
 {
-    string name;
-    int countWorkshop;
-    int countWorkshopInWork;
-    string other;
+    string name; // название
+    int countWorkshop; // количество цехов
+    int countWorkshopInWork; // количество цехов в работе
+    string other; // класс станции
 };
 
+int InputInt() {
+    int num;
+    while (true) {
+        cin >> num;
+        if (cin.fail() or num <= 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Ошибка: Введено некорректное значение. Попробуйте снова" << endl;
+            cout << "Ввод: ";
+        }
+        else {
+            return num;
+        }
+    }
+}
+
+float InputFloat() {
+    float num;
+    while (true) {
+        cin >> num;
+        if (cin.fail() or num <= 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Ошибка: Введено некорректное значение. Попробуйте снова" << endl;
+            cout << "Ввод: ";
+        }
+        else {
+            return num;
+        }
+    }
+}
+
 void menu() {
+    setlocale(LC_ALL, "Russian");
     while (true)
     {
         int number;
+
+        cout << "1. Добавить трубу" << endl;
+        cout << "2. Добавить КС" << endl;
+        cout << "3. Просмотр всех объектов" << endl;
+        cout << "4. Редактировать трубу" << endl;
+        cout << "5. Редактировать КС" << endl;
+        cout << "6. Сохранить" << endl;
+        cout << "7. Загрузить" << endl;
+        cout << "8. Выход" << endl;
+        cout << "Выберите действие: ";
+
+        number = InputInt();
 
         switch (number) {
         case 1:
@@ -61,22 +107,14 @@ void menu() {
 
             break;
         }
-        case 8:
-        {
-
-            break;
-        }
-        case 9:
-        {
-
-            break;
-        }
         case 0:
         {
-
+            cout << "Выход из программы!";
+        }
+        default:
+            cout << "Попробуйте ввести ещё раз, такого числа нет!\n" << endl;
             break;
-        }
-        }
+        };
     };
 };
 
